2fc5b53336097dd23b1a48a6fa5e7638246eb6d6
Kiran Gadhave <kirangadhave2@gmail.com>
2019-09-17 17:55:43 -0600

    Added zsh

--- zsh/zsh_setup.zsh
+++ zsh/zsh_setup.zsh
@@ -5,13 +5,16 @@ source $DOTFILES_HOME/seperator.zsh
 export ZPLUGIN="$ZSH_HOME/zplugin/bin"
 
 if [ ! -f "$ZSH_HOME/ran_once" ]; then
-    echo "ran_once not present"
     touch $ZSH_HOME/ran_once
     git submodule update --init --recursive
 else
-    echo "ran_once present"
     git pull --recurse-submodules
 fi
+
+ln -sf $ZSH_HOME/zshrc $HOME/.zshrc
+
+source $HOME/.zshrc
+
 source $DOTFILES_HOME/seperator.zsh
 echo "Setting up ZSH"
 source $DOTFILES_HOME/seperator.zsh
